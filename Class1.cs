e26579dab4040c6262e249fffe99571b1e807c7e
lokotskydima <isip_d.o.lokockiy@mpt.ru>
2022-11-21 21:46:36 +0300

    init

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1lokotskiy
+{
+
+    public class Class1
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+
+    }
+}
